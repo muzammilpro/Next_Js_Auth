f24fe17125883b76f28013f6be29516ac94920ac
muzammilpro <muzammilhusnain7@gmail.com>
2023-08-25 12:59:04 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -14,9 +14,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          
         </a>
       </div>
     </main>
