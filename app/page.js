d283804078b0134e10f5e94aaabc59e880db8794
muzammilpro <muzammilhusnain7@gmail.com>
2023-08-27 13:01:52 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -5,10 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
   
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      
-       
-      </div>
+     
     </main>
   )
 }
