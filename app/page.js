c5dc874f54bd98649ead115b3ffcfdde30caea98
muzammilpro <muzammilhusnain7@gmail.com>
2023-09-05 13:50:56 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
   <div className='grid place-items-center h-screen'>
-    <div className='text-xl font-bold my-4'>
-    <h1>Enter the detail</h1>
+    <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400 '>
+    <h1 className='text-xl font-bold my-4'>Enter the detail</h1>
     </div>
   </div>
   )
